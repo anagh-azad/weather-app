e2c59fac163a845e941ab971086f8c75626fd40c
anagh-azad <anagh.azad.ug20@nsut.ac.in>
2023-11-09 14:19:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect,useState} from "react";
+import axios from "axios";
 
 function App() {
+
+  const [temp,setTemp]=useState("");
+  const [humidity,setHumidity]=useState("");
+  const [windSpeed,setWindSpeed]=useState("");
+  const [location,setLocation]=useState("Algeria");
+  const [disLocation,setdisLocation]=useState("Algeria");
+
+  //const url="https://api.openweathermap.org/data/2.5/weather?q=Algeria&APPID=794ee95e63c5a32aaf88cd813fa2e425";
+
+  const searchLocation=(event)=>{
+    if(event.key == "Enter"){
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=794ee95e63c5a32aaf88cd813fa2e425`)
+    .then((res)=>{
+      console.log(res.data);
+      setTemp(res.data.main.temp);
+      setHumidity(res.data.main.humidity);
+      setWindSpeed(res.data.wind.speed);
+      setdisLocation(location);
+    })
+  }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+
+      <div className="search">
+        <input
+        value={location}
+        onChange={event=>setLocation(event.target.value)}
+        onKeyDown={searchLocation}
+        placeholder="Enter Location"
+        type="text"
         >
-          Learn React
-        </a>
-      </header>
+        </input>
+      </div>
+
+      <div className="container">
+
+        <div className="top">
+          <div className="location">{disLocation}</div>
+          <div className="temp">{temp} K</div>
+        </div>
+
+        <div className="bottom">
+          <div className="humidity">Humidity : {humidity}%</div>
+          <div className="wind">Wind Speed : {windSpeed} MPH</div>
+        </div>
+
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
